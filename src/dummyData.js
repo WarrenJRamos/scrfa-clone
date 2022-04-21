d16fe709a9b97348f0fb75962060666fbec7829d
WarrenJRamos <wjnramos@gmail.com>
2022-04-21 09:40:47 -0600

    created dummy api response for home page to retrieve popular services and links, recent updates, and calendar of events

---
+++ src/dummyData.js
@@ -0,0 +1,118 @@
+export const dummyAPIResponse = {
+  popularServicesAndLinks: [
+    {
+      title: "Overview and History of the South Carolina State Budget",
+      src: "",
+    },
+    {
+      title: "Employer Contribution Trends",
+      src: "",
+    },
+    {
+      title: "Appropriation Bills and Acts",
+      src: "",
+    },
+    {
+      title: "Overview and History of the South Carolina State Budget",
+      src: "",
+    },
+    {
+      title: "Employer Contribution Trends - June 4, 2019",
+      src: "",
+    },
+    {
+      title: "Executive Budget Office State Agency Budget Plans(Requests)",
+      src: "",
+    },
+    {
+      title: "Overview and History of the South Carolina State Budget",
+      src: "",
+    },
+    {
+      title: "Employer Contribution Trends - June 4, 2019",
+      src: "",
+    },
+    {
+      title: "Executive Budget Office State Agency Budget Plan(Requests)",
+      src: "",
+    },
+  ],
+  recentUpdates: [
+    {
+      title: "Local Government Finance",
+      src: "",
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean a leo pulvinar, dignissim ex ac, eleifend est. ",
+      datePublished: "Jan 2, 2020",
+      status: "Updated",
+    },
+    {
+      title: "Transportation Netwrok Carrier Maps",
+      src: "",
+      description: "",
+      datePublished: "Jan 2, 2020",
+      status: "New",
+    },
+    {
+      title:
+        "BEA Long-Range General Fund Revenue Plan for FY 2019-20 to FY 2022-23",
+      src: "",
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean a leo pulvinar, dignissim ex ac, eleifend est. ",
+      datePublished: "Dec 16, 2019",
+      status: "Updated",
+    },
+    {
+      title: "Budget Development",
+      src: "",
+      description: "",
+      datePublished: "Dec 16, 2019",
+      status: "New",
+    },
+    {
+      title: "November 2019 General Fund Revenue Digest",
+      src: "",
+      description: "",
+      datePublished: "Dec 13, 2019",
+      status: "New",
+    },
+  ],
+  calendarOfEvents: [
+    {
+      eventDate: "Nov 22",
+      eventTitle: "Board of Economic Advisors Meeting",
+      eventLocation:
+        "Room 417, Bowers Conference Room, Rembert Dennis Building",
+      eventTime: "1:30pm",
+      eventContact: "(803) 734-2265",
+      eventStatus: "Updated",
+    },
+    {
+      eventDate: "Nov 23",
+      eventTitle: "Board of Economic Advisors Meeting",
+      eventLocation:
+        "Room 417, Bowers Conference Room, Rembert Dennis Building",
+      eventTime: "1:30pm",
+      eventContact: "(803) 734-2265",
+      eventStatus: "Cancelled",
+    },
+    {
+      eventDate: "Nov 25",
+      eventTitle: "Board of Economic Advisors Meeting",
+      eventLocation:
+        "Room 417, Bowers Conference Room, Rembert Dennis Building",
+      eventTime: "1:30pm",
+      eventContact: "(803) 734-2265",
+      eventStatus: "Updated",
+    },
+    {
+      eventDate: "Nov 28",
+      eventTitle: "Board of Economic Advisors Meeting",
+      eventLocation:
+        "Room 417, Bowers Conference Room, Rembert Dennis Building",
+      eventTime: "1:30pm",
+      eventContact: "(803) 734-2265",
+      eventStatus: "Updated",
+    },
+  ],
+};
