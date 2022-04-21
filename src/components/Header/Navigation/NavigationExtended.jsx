4ec716f3f99118d22ff0ed9b26159040b133d545
WarrenJRamos <wjnramos@gmail.com>
2022-04-20 22:27:46 -0600

    added more content to the expanded nav

--- src/components/Header/Navigation/NavigationExtended.jsx
+++ src/components/Header/Navigation/NavigationExtended.jsx
@@ -9,28 +9,80 @@ export const NavigationExtended = (props) => {
   return (
     <NavigationExtendedContainer>
       <div className="side-menu">
-        <ul className="general-menu__list">
-          <li className="general-menu__list-item">
+        <ul className="side-menu__list">
+          <li className="side-menu__list-item">
             <a href="">Home</a>
           </li>
-          <li className="general-menu__list-item">
+          <li className="side-menu__list-item">
             <a href="">News & Events</a>
           </li>
-          <li className="general-menu__list-item">
+          <li className="side-menu__list-item">
             <a href="">About Us</a>
           </li>
-          <li className="general-menu__list-item">
+          <li className="side-menu__list-item">
             <a href="">Contact Us</a>
           </li>
-          <li className="general-menu__list-item">
+          <li className="side-menu__list-item">
             <a href="">Boards & Committees</a>
           </li>
         </ul>
-        <div className="robust-menu">
-          <div className="robust-menu__section-a"></div>
-          <div className="robust-menu__section-b"></div>
-          <div className="robust-menu__section-c"></div>
-          <div className="robust-menu__section-d"></div>
+      </div>
+      <div className="robust-menu">
+        <div className="robust-menu__section">
+          <div className="list-header">Data & Research</div>
+          <ul className="robust-menu__list">
+            <li className="robust-menu__list-item">Economy</li>
+            <li className="robust-menu__list-item">State Finances</li>
+            <li className="robust-menu__list-item">Education</li>
+            <li className="robust-menu__list-item">Healthcare</li>
+            <li className="robust-menu__list-item">Local Government</li>
+            <li className="robust-menu__list-item">
+              Population and Demographics
+            </li>
+            <li className="robust-menu__list-item">
+              State Register Calculations
+            </li>
+            <li className="robust-menu__list-item">Presentations</li>
+          </ul>
+        </div>
+        <div className="robust-menu__section">
+          <div className="list-header">Geography & Mapping</div>
+          <ul className="robust-menu__list">
+            <li className="robust-menu__list-item">
+              Transportation Carrier Network (Uber)
+            </li>
+            <li className="robust-menu__list-item">SC Real Time Network</li>
+            <li className="robust-menu__list-item">SC State GIS</li>
+            <li className="robust-menu__list-item">Maps</li>
+            <li className="robust-menu__list-item">Interactive Mapping</li>
+            <li className="robust-menu__list-item">Statewide Aerial Imagery</li>
+          </ul>
+        </div>
+        <div className="robust-menu__section">
+          <div className="list-header">Programs & Services</div>
+          <ul className="robust-menu__list">
+            <li className="robust-menu__list-item">
+              Precinct Demographics and Redistricting
+            </li>
+            <li className="robust-menu__list-item">State 911 Program</li>
+            <li className="robust-menu__list-item">
+              Data Services and Online Solutions
+            </li>
+            <li className="robust-menu__list-item">Geodetic Survey</li>
+            <li className="robust-menu__list-item">Fiscal Analysis</li>
+            <li className="robust-menu__list-item">2020 Census</li>
+          </ul>
+        </div>
+        <div className="robust-menu__section">
+          <div className="list-header">Legislative Fiscal Impacts</div>
+          <ul className="robust-menu__list">
+            <li className="robust-menu__list-item">
+              Current General Assembly Session
+            </li>
+            <li className="robust-menu__list-item">
+              Previous General Assembly Session
+            </li>
+          </ul>
         </div>
       </div>
     </NavigationExtendedContainer>
--- src/styles/Header/Navigation/NavigationExtended.styled.js
+++ src/styles/Header/Navigation/NavigationExtended.styled.js
@@ -1,6 +1,62 @@
 import styled from "styled-components";
 
 export const NavigationExtendedContainer = styled.div`
-  height: 300px;
-  background-color: red;
+  border-bottom: 10px solid #1b3a61;
+  display: flex;
+  gap: 20px;
+
+  .side-menu {
+    /* FIXME */
+    background-color: #f3f6f9;
+    padding: 30px;
+    flex-basis: 200px;
+
+    &__list {
+      list-style: none;
+
+      &-item {
+        margin-bottom: 10px;
+        padding: 0 0 10px;
+        border-bottom: 1px solid #8c6910;
+        text-align: center;
+
+        a {
+          text-decoration: none;
+          /* FIXME */
+          color: black;
+        }
+      }
+    }
+  }
+
+  .robust-menu {
+    padding: 30px 14px;
+    display: flex;
+    justify-content: space-between;
+    flex-wrap: wrap;
+    row-gap: 30px;
+
+    &__section {
+      .list-header {
+        font-weight: 600;
+
+        &::after {
+          margin: 7px 0 12px;
+          content: "";
+          display: block;
+          height: 3px;
+          width: 50px;
+          background-color: #8c6910;
+        }
+      }
+    }
+
+    &__list {
+      list-style: none;
+
+      &-item {
+        margin-bottom: 10px;
+      }
+    }
+  }
 `;
